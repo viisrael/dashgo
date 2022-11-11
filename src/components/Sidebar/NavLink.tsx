f720b43dab5f4492eeb1ae12f976506b39e9592a
Vially Israel <viallyisrael@hotmail.com>
2022-11-11 11:14:34 -0300

    Cap 4, Unit 1- Building future interfaces, Lesson 4 -Responsiviness, video 9

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
-import Link from "next/link";
 import { ElementType } from "react";
+import { ActiveLink } from "../ActiveLink";
 
 interface NaveLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -10,11 +10,11 @@ interface NaveLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href,...rest }: NaveLinkProps) {
   return (
-    <Link href={ href } passHref>
+    <ActiveLink href={ href } passHref>
       <ChakraLink display="flex" alignContent="center" {...rest }>
         <Icon as={ icon } fontSize="20" />
         <Text ml="4" fontWeight="medium">{ children }</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
